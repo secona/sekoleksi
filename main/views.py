90f05713a47e8081fdbd3cc0ac955852fc874e00
secona <77039267+secona@users.noreply.github.com>
2024-09-09 20:56:02 +0700

    feat: make views and models

--- main/views.py
+++ main/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def show_main(request):
-    return render(request, "main.html", {})
+    return render(request, "main.html", {
+        "name": "Muhammad Vito Secona",
+        "npm": "2306152411",
+        "class": "F",
+    })
