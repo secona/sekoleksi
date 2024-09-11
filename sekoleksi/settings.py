401a4a7cb243434e1998f0af01c352227d827236
secona <77039267+secona@users.noreply.github.com>
2024-09-11 11:46:49 +0700

    feat: update allowed_hosts

--- sekoleksi/settings.py
+++ sekoleksi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8z$#jlssjcc)3spwh97*anr-x9xug%+l5=%73&0y@rt78dvmnh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "muhammad-vito31-sekoleksi.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "muhammad-vito31-sekoleksi1.pbp.cs.ui.ac.id"]
 
 
 # Application definition
