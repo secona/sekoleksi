fb8c873d8e688c14b80d161dc56b7b5d6eae6fda
secona <77039267+secona@users.noreply.github.com>
2024-09-16 13:50:01 +0700

    feat: input form for products

--- main/views.py
+++ main/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ProductForm
+from .models import Product
 
 def show_main(request):
+    products = Product.objects.all()
+
     return render(request, "main.html", {
         "name": "Muhammad Vito Secona",
         "npm": "2306152411",
         "class": "F",
+        "products": products,
     })
+
+def create_product(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == 'POST':
+        form.save()
+        return redirect('main:show_main')
+
+    return render(request, 'create_product.html', { 'form': form })
